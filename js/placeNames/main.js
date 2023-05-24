bf3e12448bd2ca9c4690f88bdb8d7989902a104d
David Profitt <dlprofitt@gmail.com>
2023-05-24 16:11:44 -0400

    Created seperate array of place names that start with 'The' and then iterated through the array, console loggin each

--- js/placeNames/main.js
+++ js/placeNames/main.js
@@ -1,12 +1,26 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
 console.log("All Pace Names");
 
 for (const name of names) {
   console.log(name);
+}
+
+console.log("'The' Place Names");
+
+let theNames = [];
+
+for (const theName of names) {
+  if (theName.startsWith("The")) {
+    theNames.push(theName);
+  }
+}
+
+for (const singleTheName of theNames) {
+  console.log(singleTheName);
 }
